38f00f00f7f014aa6b4eb8fa40cea6492cd17de1
SrZitu <zitu094@gmail.com>
2023-08-02 20:33:26 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+import { ref } from 'vue'
+const color = ref('none');
+
+function getStyle() {
+  return {
+    backgroundColor: color.value
+  }
+}
+
+</script>
+
+<template>
+  <section class="mx-auto container">
+    <h1 class="text-2xl mb-10">Vue Form</h1>
+    <p class="pb-5">Your Favorite Color:{{ color }}</p>
+    <p>
+      Your Favorite Color: {{ color }}
+      <!-- 2 way data binding using v-model -->
+      <input class="p-5" v-model="color" /> 
+    <div class="w-32 h-32 mt-10" :style="getStyle()"></div>
+    </p>
+  </section>
+</template>
+<style scoped></style>
