a623d6668cfe10db9308a5f39c62975b0400d581
SrZitu <zitu094@gmail.com>
2023-08-07 12:42:54 +0600

    dynamic search

--- src/App.vue
+++ src/App.vue
@@ -1,43 +1,33 @@
 <script setup>
-import { ref,reactive,computed } from 'vue'
+import { ref, reactive, computed } from 'vue'
+import { countries } from "./data/countries.js";
 
-const persons = reactive([
-  { name: 'John Doe', age: 25, email: 'john@doe.com' },
-  { name: 'Jane Doe', age: 26, email: 'jane@doe.com' },
-  { name: 'John Smith', age: 27, email: 'john@smith.com' },
-  { name: 'Jane Smith', age: 28, email: 'jane@smith.com' },
-  { name: 'sazzadur Rahman', age: 29, email: 'zitu094@smith.com' },
-  
-]);
+const search = ref('');
 
-const activeIndex=ref(0);
-
-
+function getCountries() {
+  return countries.filter((country) => {
+    return country.name.toLowerCase().startsWith(search.value);
+  });
+}
 </script>
 
 <template>
   <section class="mx-auto container text-left">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="my-5">{{ persons }}</p>
-    <div class="flex ">
-      <div class="w-1/2">
-        <p>{{activeIndex}}</p>
-        
-        <p class="mb-10">
-        <h2>Select a person</h2>
-        <select name="person" id="person" v-model="activeIndex">
-          <option :value="index" v-for="(person,index) in persons" :key="index" >{{ person.name }}</option>
-        </select>
-        </p>
-      </div>
-      <div>
-        <div class="flex flex-col"  >
-          <input class=" my-3" type="text"  :placeholder="key" v-model="persons[activeIndex][key]" v-for="(value,key,index2) in persons[activeIndex]" :key="key"  >
-        </div>
-      </div>
-    </div>
+    <p class="my-5">{{ search }}</p>
+    <p class="my-5">
+      search: <input class="p-3" type="text" v-model="search">
+    </p>
+    <p class="mt-10" v-for="country in getCountries()" :key="country.code">
+    <ul>
+      <li>
+{{ country.name }}
+        <!-- <span class='text-red-500'>{{ search }}</span>{{country.name.toLowerCase().replace(search,'')}} -->
+      </li>
+    </ul>
+    </p>
+
 
   </section>
-
 </template>
 <style scoped></style>
