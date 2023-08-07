e7c219286871b116acf2c4f783a67992a17ac0ab
SrZitu <zitu094@gmail.com>
2023-08-07 09:32:59 +0600

    quick app

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,48 @@
 <script setup>
-import { ref,reactive } from 'vue'
-const favColors=ref([]);
+import { ref,reactive,computed } from 'vue'
+
+const saarcCountries = reactive([
+  { name: "Bangladesh", capital: "Dhaka" },
+  { name: "Nepal", capital: "Kathmandu" },
+  { name: "Bhutan", capital: "Thimphu" },
+  { name: "Sri Lanka", capital: "Colombo" },
+  { name: "India", capital: "New Delhi" },
+  { name: "Pakistan", capital: "Islamabad" },
+  { name: "Maldives", capital: "Male" }
+]);
+const capitals = ["Dhaka", "Kathmandu", "Thimphu", "Colombo", "New Delhi", "Islamabad", "Male"];
+
+
+const getScore = computed(() => {
+  let score = 0;
+  saarcCountries.forEach(country => {
+    if (country.capital === country.answer) {
+      score++;
+    }
+  });
+  return score;
+});
+
+function getCapitals(){
+  return capitals;
+  //return capitals.sort(()=>Math.random()-0.5);
+}
 
 </script>
 
 <template>
-  <section class="mx-auto container">
+   <section class="mx-auto container text-left">
     <h1 class="text-2xl mb-10">Vue Form</h1>
-    <p class="mb-10">{{ favColors }}</p>
-    Your favorite colors <br/>
-    <label for="red">Red</label>
-    <input class="ml-2 mr-2" type="checkbox" id="red" v-model="favColors" value="red">
-    <label for="green">Green</label>
-    <input class="ml-2 mr-2" type="checkbox" id="green" v-model="favColors" value="green" >
-    <label for="blue">Blue</label>
-    <input class="ml-2 mr-2" type="checkbox" id="blue" v-model="favColors" value="blue">
-     <label for="yellow">yellow</label>
-    <input class="ml-2 mr-2" type="checkbox" id="blue" v-model="favColors" value="yellow">
-    
+    <p class="mb-10">{{ saarcCountries }}</p>
+    <p>Your Score: {{ getScore }}</p>
+    <div class="my-5 border border-gray-400 p-5" v-for="(country,index) in saarcCountries" :key="index">
+      <p>What is the capital of {{ country['name'] }}  ?</p>
+      <template  v-for="(capital,index) in getCapitals()" :key="index">
+        <input  :name="country.name" type="radio" :value="capital" v-model="country.answer" />
+        <label class="ml-2 mr-2">{{ capital }}</label>
+      </template>
+    </div>
   </section>
+
 </template>
 <style scoped></style>
